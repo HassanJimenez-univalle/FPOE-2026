146f26d5674d9c1805f7536f2d14ee365ae15a65
Hassan Jimenez <hassan.jimenez@correounivalle.edu.co>
2026-05-27 17:12:37 -0500

    Creacion del proyecto

--- src/main/java/web/ControladorAsignaturas.java
+++ src/main/java/web/ControladorAsignaturas.java
@@ -6,8 +6,15 @@ package web;
 
 import Modelo.Asignatura;
 import jakarta.enterprise.context.SessionScoped;
+import jakarta.faces.application.FacesMessage;
+import jakarta.faces.context.FacesContext;
+import jakarta.inject.Inject;
 import jakarta.inject.Named;
 import java.io.Serializable;
+import java.sql.SQLIntegrityConstraintViolationException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import logica.ILogica;
 
 /**
  *
@@ -17,12 +24,23 @@ import java.io.Serializable;
 @SessionBean
 @SessionScoped
 public class ControladorAsignaturas implements Serializable {
-    
-    private Asignatura asignatura = new  Asignatura("750014C", "FPOE",(byte)3, (byte)3);
+
+    private Asignatura asignatura = new Asignatura("750014C", "FPOE", (byte) 3, (byte) 3);
+
+    @Inject
+    private ILogica logica;
 
     public Asignatura getAsignatura() {
         return asignatura;
     }
     
-    
+    public void guardar() throws Exception{
+        try {
+            this.logica.crearAsignatura(this.asignatura);
+            FacesContext.getCurrentInstance().addMessage(
+                    null, new FacesMessage("La asignatura fue guardada"));
+        } catch (SQLIntegrityConstraintViolationException ex) {
+            Logger.getLogger(ControladorAsignaturas.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
 }
